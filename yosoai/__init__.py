f8534d96e976a6314884c393037c8d972abdc3b8
VinciGit00 <mvincig11@gmail.com>
2024-02-10 16:50:12 +0100

    refactoring

--- yosoai/__init__.py
+++ yosoai/__init__.py
@@ -1,5 +1,5 @@
 from .class_creator import create_class
 from .class_generator import Generator
-from .getter import remover, get_function
+from .getter import  get_function
 from .token_calculator import truncate_text_tokens
 from .request import send_request
--- yosoai/request.py
+++ yosoai/request.py
@@ -1,7 +1,6 @@
 import time
 from tqdm import tqdm 
 from typing import List
-from .getter import remover
 from .class_generator import Generator
 from .class_creator import create_class
 from .token_calculator import truncate_text_tokens
@@ -38,7 +37,9 @@ def send_request(key: str, text:str, values:list[dict], model:str, temperature:f
     create_class(values)
     time.sleep(2)  # TODO: implement asynchronous waiting
 
-    messages = truncate_text_tokens(text.replace("\\n", ""), model, encoding_name)
+    print(text.replace("\\n", ""))
+
+    messages = truncate_text_tokens(text, model, encoding_name)
     
     processed_messages = 0
 
