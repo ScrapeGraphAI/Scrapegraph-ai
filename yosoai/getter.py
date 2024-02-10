5bd3733da0a20cf8615c8925ccc070733118a8cd
VinciGit00 <mvincig11@gmail.com>
2024-02-10 17:11:59 +0100

    removed unused library

--- yosoai/getter.py
+++ yosoai/getter.py
@@ -1,5 +1,4 @@
 from langchain_community.document_loaders import AsyncHtmlLoader 
-from langchain_community.document_transformers import Html2TextTransformer
 
 def get_function(link:str) -> str:
     """
