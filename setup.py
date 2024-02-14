c50bb55bcfa88759a30ec994197cbebfb4548b82
Marco Perini <perinim.98@gmail.com>
2024-02-15 00:00:09 +0100

    fix: fixed typo doc url

--- setup.py
+++ setup.py
@@ -15,10 +15,10 @@ dev_requirements = load_requirements('requirements-dev.txt')
 setup(
     name='scrapegraphai',
     version='0.1.0',  # MAJOR.MINOR.PATCH
-    description='A web scraping library using langchain',
+    description='A web scraping library based on LangChain which uses LLM and direct graph logic to create scraping pipelines.
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    url='https://yoso-ai.readthedocs.io/',
+    url='https://scrapegraph-ai.readthedocs.io/',
     author='Marco Vinciguerra',
     author_email='mvincig11@gmail.com',
     license='Apache 2.0',
