f79dee802f342d47e754a39f1d2e2cee14c3749d
VinciGit00 <mvincig11@gmail.com>
2024-02-15 23:01:24 +0100

    changed score evaluation

--- .github/workflows/evaluate_pylint_score.py
+++ .github/workflows/evaluate_pylint_score.py
@@ -2,9 +2,8 @@
 
 import sys
 
-def evaluate_pylint_score(output_file):
-    with open(output_file, 'r') as f:
-        lines = f.readlines()
+def evaluate_pylint_score():
+    lines = sys.stdin.readlines()
     
     score_line = next((line for line in lines if 'Your code has been rated at' in line), None)
     if score_line:
@@ -20,8 +19,4 @@ def evaluate_pylint_score(output_file):
         sys.exit(1)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python evaluate_pylint_score.py <pylint_output_file>")
-        sys.exit(1)
-    output_file = sys.argv[1]
-    evaluate_pylint_score(output_file)
+    evaluate_pylint_score()
