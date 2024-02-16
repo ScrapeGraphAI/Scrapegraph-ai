4e509e1f664628e4fd71dccc63b6f5b7a3eb60da
VinciGit00 <mvincig11@gmail.com>
2024-02-16 19:43:48 +0100

    removed unused files

--- scrapegraphai/__init__.py
+++ scrapegraphai/__init__.py
@@ -1,8 +1,3 @@
 """
     __init__.py file for scrapegraphai folder
 """
-from .class_creator import create_class
-from .class_generator import Generator
-from .getter import _get_function
-from .request import send_request
-from .utils.convert_to_csv import convert_to_csv
--- scrapegraphai/pydantic_class.py
+++
@@ -1,8 +0,0 @@
-"""
-Pydantic module to send
-"""
-from langchain_core.pydantic_v1 import BaseModel, Field
-
-
-class _Response(BaseModel):
-    title_news: str = Field(description='Give me the name of the news')
