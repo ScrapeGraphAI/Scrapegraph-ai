5f20299cf34e2d6f0f70c0a1b2e512c180fab5d9
VinciGit00 <mvincig11@gmail.com>
2024-03-07 13:22:21 +0100

    add tests

--- tests/graphs/smart_scraper_test.py
+++ tests/graphs/smart_scraper_test.py
@@ -1,37 +1,37 @@
-""" 
-Module for testing the class SmartScraperGraph
-"""
-import unittest
 import os
+import unittest
 from dotenv import load_dotenv
 from scrapegraphai.graphs import SmartScraperGraph
 
+load_dotenv()
 
-class TestSmartScraperGraph(unittest.TestCase):
-    """ 
-    class for testing the class SmartScraperGraph
-    """
 
-    @classmethod
-    def setUpClass(cls):
-        load_dotenv()
+class TestScrapingPipeline(unittest.TestCase):
+    """Test class for SmartScraperGraph"""
+
+    def setUp(self):
+        # Define the configuration for the language model
         openai_key = os.getenv("OPENAI_APIKEY")
-        cls.llm_config = {
+        llm_config = {
             "api_key": openai_key,
             "model_name": "gpt-3.5-turbo",
         }
-        cls.URL = "https://perinim.github.io/projects/"
-        cls.PROMPT = "List me all the titles and project descriptions and give me an audio"
-        cls.smart_scraper_graph = SmartScraperGraph(
-            cls.PROMPT, cls.URL, cls.llm_config)
 
-    def test_scraper_execution(self):
-        """ 
-        Execution of the test
-        """
-        answer = self.smart_scraper_graph.run()
-        self.assertIsNotNone(answer)
-        self.assertNotEqual(answer, "")
+        # Define URL and PROMPT
+        url = "https://perinim.github.io/projects/"
+        prompt = "List me all the titles and project descriptions"
+
+        # Create the SmartScraperGraph instance
+        self.smart_scraper_graph = SmartScraperGraph(prompt, url, llm_config)
+
+    def test_scraping(self):
+        """Main test method"""
+        # Run the scraper
+        result = self.smart_scraper_graph.run()
+        print(result)
+
+        # Check if the result is not empty
+        self.assertNotEqual(result, {})
 
 
 if __name__ == '__main__':
--- tests/graphs/speech_summary_test.py
+++ tests/graphs/speech_summary_test.py
@@ -1,41 +1,42 @@
-""" 
-Module for testing the class SpeechSummaryGraph
-"""
-import unittest
 import os
+import unittest
 from dotenv import load_dotenv
 from scrapegraphai.graphs import SpeechSummaryGraph
 
+load_dotenv()
+
 
 class TestSpeechSummaryGraph(unittest.TestCase):
-    """ 
-    class for testing the class SpeechSummaryGraph
-    """
+    """Test class for SpeechSummaryGraph"""
 
     def setUp(self):
-        load_dotenv()
+        # Define the configuration for the language model
         openai_key = os.getenv("OPENAI_APIKEY")
-        self.llm_config = {
+        llm_config = {
             "api_key": openai_key,
         }
-        self.curr_dir = os.path.dirname(os.path.realpath(__file__))
-        self.output_file_path = os.path.join(
-            self.curr_dir, "website_summary.mp3")
+
+        # Save the audio to a file
+        curr_dir = os.path.dirname(os.path.realpath(__file__))
+        output_file_path = os.path.join(curr_dir, "website_summary.mp3")
+
+        # Initialize SpeechSummaryGraph instance
+        self.speech_summary_graph = SpeechSummaryGraph(
+            "Make a summary of the news to be converted to audio for blind people.",
+            "https://www.wired.com/category/science/",
+            llm_config,
+            output_file_path
+        )
 
     def test_summary_generation(self):
-        """ 
-        Execution of the test
-        """
-        speech_summary_graph = SpeechSummaryGraph("""Make a summary of the news to be
-                                                    converted to audio for 
-                                                    blind people.""",
-                                                  "https://www.wired.com/category/science/",
-                                                  self.llm_config,
-                                                  self.output_file_path)
-        final_state = speech_summary_graph.run()
-        result = final_state.get("answer", "No answer found.")
-        self.assertIsNotNone(result)
-        self.assertNotEqual(result, "No answer found.")
+        """Test summary generation and audio conversion"""
+        # Run the summary graph
+        final_state = self.speech_summary_graph.run()
+        print(final_state)
+
+        # Check if the final state contains non-empty 'answer' key
+        self.assertTrue(final_state.get("answer")
+                        is not None and final_state["answer"] != "")
 
 
 if __name__ == '__main__':
