f27e0b4c5ffca4104d9ee4dc3e638437702d9e9d
Perinim <perinim.98@gmail.com>
2024-03-17 17:01:29 +0100

    removed fetch_html and fetch_text (merged)

--- scrapegraphai/nodes/__init__.py
+++ scrapegraphai/nodes/__init__.py
@@ -1,7 +1,7 @@
 """ 
 __init__.py file for node folder 
 """
-from .fetch_html_node import FetchHTMLNode
+
 from .fetch_node import FetchNode
 from .conditional_node import ConditionalNode
 from .get_probable_tags_node import GetProbableTagsNode
@@ -10,4 +10,3 @@ from .parse_node import ParseNode
 from .rag_node import RAGNode
 from .text_to_speech_node import TextToSpeechNode
 from .image_to_text_node import ImageToTextNode
-from .fetch_text_node import FetchTextNode
--- scrapegraphai/nodes/text_to_speech_node.py
+++ scrapegraphai/nodes/text_to_speech_node.py
@@ -4,7 +4,7 @@ Module for parsing the text to voice
 """
 
 from .base_node import BaseNode
-
+from typing import List
 
 class TextToSpeechNode(BaseNode):
     """
@@ -17,14 +17,14 @@ class TextToSpeechNode(BaseNode):
         execute(state, text): Execute the node's logic and return the updated state.
     """
 
-    def __init__(self, llm, node_name: str = "TextToSpeechNode"):
+    def __init__(self, input: str, output: List[str], model_config: dict, node_name: str = "TextToSpeechNode"):
         """
         Initializes an instance of the TextToSpeechNode class.
         """
-        super().__init__(node_name, "node")
-        self.llm = llm
+        super().__init__(node_name, "node", input, output, 1, model_config)
+        self.text2speech_model = model_config["text2speech_model"]
 
-    def execute(self, state: dict) -> dict:
+    def execute(self, state):
         """
         Execute the node's logic and return the updated state.
         Args:
@@ -34,11 +34,21 @@ class TextToSpeechNode(BaseNode):
         :return: The updated state after executing this node.
         """
 
-        text2translate = state.get("answer", None)
-        if not text2translate:
+        print(f"--- Executing {self.node_name} Node ---")
+    
+        # Interpret input keys based on the provided input expression
+        input_keys = self.get_input_keys(state)
+        
+        # Fetching data from the state based on the input keys
+        input_data = [state[key] for key in input_keys]
+
+        text2translate = input_data[0]
+
+        # if not a string, raise an error
+        if not isinstance(text2translate, str):
             raise ValueError("No text to translate to speech.")
         print("---TRANSLATING TEXT TO SPEECH---")
-        audio = self.llm.run(text2translate["summary"])
+        audio = self.text2speech_model.run(text2translate["summary"])
 
-        state.update({"audio": audio})
+        state.update({self.output[0]: audio})
         return state
