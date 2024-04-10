4ef1564e189a9c745e76813d3234b9513313ef3a
VinciGit00 <mvincig11@gmail.com>
2024-04-10 12:49:55 +0200

    Update smart_scraper_gemini.py

--- examples/gemini/smart_scraper_gemini.py
+++ examples/gemini/smart_scraper_gemini.py
@@ -12,11 +12,11 @@ load_dotenv()
 # Define the configuration for the graph
 # ************************************************
 
-openai_key = os.getenv("OPENAI_APIKEY")
+gemini_key = os.getenv("GOOGLE_APIKEY")
 
 graph_config = {
     "llm": {
-        "api_key": openai_key,
+        "api_key": gemini_key,
         "model": "gpt-3.5-turbo",
     },
 }
