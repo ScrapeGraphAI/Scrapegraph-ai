0bb68d1500bc69cdfb5b36c35582c67e164e42b2
VinciGit00 <mvincig11@gmail.com>
2024-05-09 10:31:21 +0200

    Merge branch 'main' of https://github.com/VinciGit00/Scrapegraph-ai

--- scrapegraphai/graphs/abstract_graph.py
+++ scrapegraphai/graphs/abstract_graph.py
@@ -7,6 +7,7 @@ from langchain_openai import AzureOpenAIEmbeddings, OpenAIEmbeddings
 from langchain_community.embeddings import HuggingFaceHubEmbeddings, OllamaEmbeddings
 from ..helpers import models_tokens
 from ..models import AzureOpenAI, Bedrock, Gemini, Groq, HuggingFace, Ollama, OpenAI, Anthropic
+from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
 
 
 class AbstractGraph(ABC):
