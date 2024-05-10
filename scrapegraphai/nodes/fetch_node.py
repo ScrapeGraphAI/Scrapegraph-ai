f8ce3d5916eab926275d59d4d48b0d89ec9cd43f
mayurdb <mayurdb31@gmail.com>
2024-05-10 13:28:53 +0530

    fix: Augment the information getting fetched from a webpage

--- scrapegraphai/nodes/fetch_node.py
+++ scrapegraphai/nodes/fetch_node.py
@@ -6,7 +6,9 @@ from typing import List, Optional
 from langchain_community.document_loaders import AsyncChromiumLoader
 from langchain_core.documents import Document
 from .base_node import BaseNode
-from ..utils.remover import remover
+from ..utils.cleanup_html import cleanup_html
+import requests
+from bs4 import BeautifulSoup
 
 
 class FetchNode(BaseNode):
@@ -32,6 +34,7 @@ class FetchNode(BaseNode):
     def __init__(self, input: str, output: List[str], node_config: Optional[dict]=None, node_name: str = "Fetch"):
         super().__init__(node_name, "node", input, output, 1)
 
+        self.useSoup = True if node_config is None else node_config.get("useSoup", True)
         self.headless = True if node_config is None else node_config.get("headless", True)
         self.verbose = False if node_config is None else node_config.get("verbose", False)
 
@@ -67,10 +70,22 @@ class FetchNode(BaseNode):
             })]
         # if it is a local directory
         elif not source.startswith("http"):
-            compressed_document = [Document(page_content=remover(source), metadata={
+            compressed_document = [Document(page_content=cleanup_html(source), metadata={
                 "source": "local_dir"
             })]
 
+        elif self.useSoup:
+            response = requests.get(source)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, 'html.parser')
+                links = soup.find_all('a')
+                link_urls = []
+                for link in links:
+                    if 'href' in link.attrs:
+                        link_urls.append(link['href'])
+                compressed_document = [Document(page_content=cleanup_html(soup.prettify(), link_urls))]
+            else:
+                print(f"Failed to retrieve contents from the webpage at url: {url}")
         else:
             if self.node_config is not None and self.node_config.get("endpoint") is not None:
                 
@@ -87,7 +102,7 @@ class FetchNode(BaseNode):
 
             document = loader.load()
             compressed_document = [
-                Document(page_content=remover(str(document[0].page_content)))]
+                Document(page_content=cleanup_html(str(document[0].page_content)))]
 
         state.update({self.output[0]: compressed_document})
         return state
