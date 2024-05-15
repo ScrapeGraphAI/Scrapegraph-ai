5587a64d23451a6a216000fe83b2ce1cc8f7141b
VinciGit00 <mvincig11@gmail.com>
2024-05-15 15:51:27 +0200

    fix: removed unused

--- scrapegraphai/graphs/__init__.py
+++ scrapegraphai/graphs/__init__.py
@@ -5,7 +5,6 @@ __init__.py file for graphs folder
 from .abstract_graph import AbstractGraph
 from .base_graph import BaseGraph
 from .smart_scraper_graph import SmartScraperGraph
-from .deep_scraper_graph import DeepScraperGraph
 from .speech_graph import SpeechGraph
 from .search_graph import SearchGraph
 from .script_creator_graph import ScriptCreatorGraph
@@ -15,4 +14,3 @@ from .csv_scraper_graph import CSVScraperGraph
 from .pdf_scraper_graph import PDFScraperGraph
 from .omni_scraper_graph import OmniScraperGraph
 from .omni_search_graph import OmniSearchGraph
-from .turbo_scraper import TurboScraperGraph
