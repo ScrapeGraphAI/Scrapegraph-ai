8bd8fb0859b51fa8ed07e4c6695c8a9d3456f317
Marco Vinciguerra <mvincig11@gmail.com>
2024-07-10 12:19:41 +0200

    refactoring of examples

--- examples/ernie/search_graph_ernie.py
+++ examples/ernie/search_graph_ernie.py
@@ -5,7 +5,7 @@ Example of Search Graph
 import os
 from dotenv import load_dotenv
 from scrapegraphai.graphs import SearchGraph
-from scrapegraphai.utils import convert_to_csv, convert_to_json, prettify_exec_info
+
 load_dotenv()
 
 # ************************************************
@@ -34,14 +34,3 @@ search_graph = SearchGraph(
 
 result = search_graph.run()
 print(result)
-
-# ************************************************
-# Get graph execution info
-# ************************************************
-
-graph_exec_info = search_graph.get_execution_info()
-print(prettify_exec_info(graph_exec_info))
-
-# Save to json and csv
-convert_to_csv(result, "result")
-convert_to_json(result, "result")
