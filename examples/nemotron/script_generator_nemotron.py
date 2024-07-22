8c859f8d72fdc8ffb5aaf13bf642cda709f50891
Marco Vinciguerra <mvincig11@gmail.com>
2024-07-22 19:12:50 +0200

    add nemotron examples

---
+++ examples/nemotron/script_generator_nemotron.py
@@ -0,0 +1,46 @@
+""" 
+Basic example of scraping pipeline using ScriptCreatorGraph
+"""
+
+import os
+from dotenv import load_dotenv
+from scrapegraphai.graphs import ScriptCreatorGraph
+from scrapegraphai.utils import prettify_exec_info
+
+load_dotenv()
+
+# ************************************************
+# Define the configuration for the graph
+# ************************************************
+
+nemotron_key = os.getenv("NEMOTRON_APIKEY")
+
+graph_config = {
+    "llm": {
+        "api_key": nemotron_key,
+        "model": "nvidia/meta/llama3-70b-instruct",
+    },
+    "library": "beautifulsoup"
+}
+
+# ************************************************
+# Create the ScriptCreatorGraph instance and run it
+# ************************************************
+
+script_creator_graph = ScriptCreatorGraph(
+    prompt="List me all the projects with their description.",
+    # also accepts a string with the already downloaded HTML code
+    source="https://perinim.github.io/projects",
+    config=graph_config
+)
+
+result = script_creator_graph.run()
+print(result)
+
+# ************************************************
+# Get graph execution info
+# ************************************************
+
+graph_exec_info = script_creator_graph.get_execution_info()
+print(prettify_exec_info(graph_exec_info))
+
