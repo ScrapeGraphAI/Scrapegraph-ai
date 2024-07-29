9560bc2386859ea0c584b2691966866278a32a7f
amosdinh <amos.dinh.2013@gmail.com>
2024-07-29 22:15:56 +0200

    feat:added firecrawl support

---
+++ examples/firecrawl/README.md
@@ -0,0 +1,9 @@
+# Using Firecrawl as scraping backend
+1. Specify your FIRECRAWL_API_KEY in `.env.example`.
+2. When self hosting Firecrawl, specify your FIRECRAWL_API_URL in `.env.example` . Example: http://localhost:3002.
+When self-hosting, no API key is needed.
+3. Rename `.env.example` to `.env`.
+4. Run one of the scripts in this example folder.
+
+## Self-hosting Firecrawl
+Follow the tutorial described in the repository https://github.com/mendableai/firecrawl. 
---
+++ examples/firecrawl/search_link_graph_firecrawl.py
@@ -0,0 +1,64 @@
+"""
+Example of Search Graph
+"""
+import os
+from dotenv import load_dotenv
+from scrapegraphai.graphs import SearchGraph
+from scrapegraphai.utils import convert_to_csv, convert_to_json, prettify_exec_info
+
+load_dotenv()
+FIRECRAWL_API_URL = os.getenv("FIRECRAWL_API_URLs")
+FIRECRAWL_API_KEY = os.getenv("FIRECRAWL_API_KEY")
+
+if FIRECRAWL_API_URL is not None:
+    os.environ['FIRECRAWL_API_URL'] = FIRECRAWL_API_URL
+if FIRECRAWL_API_KEY is not None:
+    os.environ['FIRECRAWL_API_KEY'] = FIRECRAWL_API_KEY
+
+
+# ************************************************
+# Define the configuration for the graph
+# ************************************************
+
+graph_config = {
+     "llm": {
+        "model": "ollama/mistral",
+        "temperature": 0,
+        "format": "json",  # Ollama needs the format to be specified explicitly
+        "base_url": "http://localhost:11434",  # set Ollama URL
+    },
+    "embeddings": {
+        "model": "ollama/nomic-embed-text",
+        "base_url": "http://localhost:11434",  # set ollama URL arbitrarily
+    },
+    "max_results": 2,
+    "verbose": True,
+    "headless":True,
+    "loader_kwargs": {
+        'scraping_backend': 'firecrawl'
+    },
+}
+
+
+# ************************************************
+# Create the SearchGraph instance and run it
+# ************************************************
+
+search_graph = SearchGraph(
+    prompt="List me the best escursions near Trento",
+    config=graph_config
+)
+
+result = search_graph.run()
+print(result)
+
+# ************************************************
+# Get graph execution info
+# ************************************************
+
+graph_exec_info = search_graph.get_execution_info()
+print(prettify_exec_info(graph_exec_info))
+
+# Save to json and csv
+convert_to_csv(result, "result")
+convert_to_json(result, "result")
