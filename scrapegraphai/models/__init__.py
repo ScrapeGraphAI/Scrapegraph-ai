5007167af1cb9a8a4f8ed9925ae765bff06017e1
Marco Vinciguerra <mvincig11@gmail.com>
2024-07-29 17:59:16 +0200

    removed unused models
    Co-Authored-By: Federico Aguzzi <62149513+f-aguzzi@users.noreply.github.com>

--- scrapegraphai/models/__init__.py
+++ scrapegraphai/models/__init__.py
@@ -1,17 +1,8 @@
 """
     __init__.py file for models folder
 """
-
-from .azure_openai import AzureOpenAI
 from .openai_itt import OpenAIImageToText
 from .openai_tts import OpenAITextToSpeech
-from .gemini import Gemini
-from .hugging_face import HuggingFace
-from .groq import Groq
-from .bedrock import Bedrock
-from .anthropic import Anthropic
 from .deepseek import DeepSeek
 from .oneapi import OneApi
-from .fireworks import Fireworks
-from .vertex import VertexAI
 from .nvidia import Nvidia
