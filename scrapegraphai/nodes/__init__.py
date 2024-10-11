1fb618cba4907699db65f63b46d1c6ffc7d9f242
Marco Vinciguerra <mvincig11@gmail.com>
2024-10-11 12:46:39 +0200

    removed unused node

--- scrapegraphai/nodes/__init__.py
+++ scrapegraphai/nodes/__init__.py
@@ -15,7 +15,6 @@ from .generate_scraper_node import GenerateScraperNode
 from .search_link_node import SearchLinkNode
 from .robots_node import RobotsNode
 from .generate_answer_csv_node import GenerateAnswerCSVNode
-from .generate_answer_pdf_node import GenerateAnswerPDFNode
 from .graph_iterator_node import GraphIteratorNode
 from .merge_answers_node import MergeAnswersNode
 from .generate_answer_omni_node import GenerateAnswerOmniNode
