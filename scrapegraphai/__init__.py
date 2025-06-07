cd29791894325c54f1dec1d2a5f6456800beb63e
Marco Vinciguerra <mvincig11@gmail.com>
2025-06-07 16:41:11 +0200

    feat: add adv

--- scrapegraphai/__init__.py
+++ scrapegraphai/__init__.py
@@ -1,3 +1,9 @@
 """
 __init__.py file for scrapegraphai folder
 """
+
+from .utils.logging import get_logger, set_verbosity_info
+
+logger = get_logger(__name__)
+set_verbosity_info()
+logger.info("This is an open-source library. If you want proxy rotations, residential proxies and faster inference times visit https://scrapegraphai.com")
