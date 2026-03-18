f47be507e642f00f94a0ac6300c0142b81c57371
octo-patch <octo-patch@users.noreply.github.com>
2026-03-18 17:32:53 +0800

    feat: upgrade MiniMax default model to M2.7
    - Add MiniMax-M2.7 and MiniMax-M2.7-highspeed to model list
    - Set MiniMax-M2.7 as default model (first in list)
    - Keep all previous models as alternatives
    - Add unit tests for MiniMax model configuration

--- scrapegraphai/helpers/models_tokens.py
+++ scrapegraphai/helpers/models_tokens.py
@@ -378,6 +378,8 @@ models_tokens = {
         "grok-beta": 128000,
     },
     "minimax": {
+        "MiniMax-M2.7": 204000,
+        "MiniMax-M2.7-highspeed": 204000,
         "MiniMax-M1": 1000000,
         "MiniMax-M1-40k": 40000,
         "MiniMax-M2": 204000,
